4982787e919e4463c18f37f3140ba5a39d4dfc09
MuaazFarooqKhan <kmuaaz18@gmail.com>
2022-07-22 15:46:42 +0500

    Add angular material and populate data

--- Angular/src/app/Components/get-posts/get-posts.component.ts
+++ Angular/src/app/Components/get-posts/get-posts.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { PostsService } from '../../services/posts.service';
+import { MatTableDataSource } from '@angular/material/table';
 
+interface PeriodicElement {
+  id: number;
+  author: string;
+  page: number;
+  rating: number;
+  title: string;
+}
 
 @Component({
   selector: 'app-get-posts',
@@ -9,13 +17,26 @@ import { PostsService } from '../../services/posts.service';
 })
 export class GetPostsComponent implements OnInit {
 
-  constructor(private _posts:PostsService) { }
+  ELEMENT_DATA: PeriodicElement[] = [];
+  displayedColumns: string[] = ['id', 'author', 'page', 'rating', 'title'];
+  dataSource:any;
+
+  constructor(private _posts: PostsService) { }
 
   ngOnInit(): void {
-    this._posts.getPosts().subscribe((posts)=>{
-      console.log(posts)
+    this._posts.getPosts().subscribe((posts) => {
+      posts.forEach((element: any) => {
+        this.ELEMENT_DATA.push({
+          id: parseInt(posts.length) + 1,
+          author: element.author,
+          page: element.page,
+          rating: element.rating,
+          title: element.title,
+        });
+      });
+      this.dataSource = new MatTableDataSource(this.ELEMENT_DATA)
     })
- 
+
   }
 
 }
--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GetPostsComponent } from './Components/get-posts/get-posts.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,12 @@ import { HttpClientModule } from '@angular/common/http';
     GetPostsComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    MatTableModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
